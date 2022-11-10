8975b6fdfcc58a072ffba97f6a2c9ff4778aa8f0
Shorya <shorya2113081@akgec.ac.in>
2022-11-11 00:50:11 +0530

    login page

--- src/Login.js
+++ src/Login.js
@@ -4,6 +4,7 @@ import "./Login.css"
 export const Login=()=>{
     return(
          <div className="Login">
+            <img src="https://api.freelogodesign.org/assets/blog/img/20180911090509731amazon_logo_RGB.jpg" alt="Amazon"></img>
             <div className="login-cont">
                 <h1>Sign In</h1>
                 <form>
@@ -14,6 +15,7 @@ export const Login=()=>{
                     <button type="submit" className="signbtn">Sign In</button>
                 </form>
                 <p>By signining-in, you agree to Amazon's Terms and Conditions</p>
+                <button className="regbtn">Create your Amazon Account</button>
             </div>
         </div>
     )
